bf5bd62da6fda981a34353139e6eb6438fa3b2de
Sherryiop <sherryiop2O@gmail.com>
2024-08-18 00:00:19 +0800

    modify training files and update model

--- Datasets/count_minions/count_minions.py
+++ Datasets/count_minions/count_minions.py
@@ -25,26 +25,11 @@ def box_center(left, top, right, bottom):
 #===========================================================#
 # 模型和影片路徑設定
 #===========================================================#
-model_path = r'C:\Users\Hi1pp\5G_YOLOv8\runs\detect\train12\weights\best.pt'
-source = r'C:\Users\Hi1pp\5G_YOLOv8\datasets\count_minions\Minion.jpg'
-output_path = r'C:\Users\Hi1pp\5G_YOLOv8\datasets\count_minions\output_minions_1.jpg'
-# frame = cv2.imread(source)
+model_path = # YOLO model path
+source = # test picture path
+output_path =  # picture output path
 model = YOLO(model_path)
-
 #===========================================================#
-# 區域框大小設定
-#===========================================================#
-current_region = None
-counting_regions = [
-    {
-        "name": "YOLOv8 Rectangle Region",
-        "polygon": Polygon([(20, 50), (500, 50), (500, 450), (20, 450)]),  # Polygon points
-        # "counts": 0,
-        "dragging": False,
-        "region_color": (37, 80, 225),  # BGR Value
-        "text_color": (0, 0, 0),  # Region Text Color
-    },
-]
 
 region_thickness = 5 #區域框線寬度
 minions_x = 0
@@ -52,12 +37,10 @@ minions_y = 0
 count_minions = 0
 names = model.model.names
 
-# results = model.predict(frame, save=True, device=0)
 results = model(source)
             
 for result in results:
     frame = result.plot()
-    # frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     for i, box in enumerate(result.boxes):
         left, top, right, bottom = np.array(box.xyxy.cpu(), dtype=np.int32).squeeze()
         center_x, center_y = box_center(left, top, right, bottom)  # 每個物件框的中心點
@@ -65,22 +48,9 @@ for result in results:
         object_name = result.names[box.cls.item()]
         if  object_name == 'minions': 
             minions_x, minions_y = center_x, center_y
-                      
-            print('helloworld')
-        count_minions += 1
+            count_minions += 1
         cv2.putText(frame, str(count_minions), (minions_x, minions_y), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,255),3)         
         
-    # if counting_regions[0]["polygon"].contains(Point(minions_x, minions_y)):
-    #     count_minions += 1
-    #     print('123')
-    #     cv2.putText(frame, count_minions, (minions_x, minions_y), cv2.FONT_HERSHEY_COMPLEX, 50, (255,0,0),20)            
-                            
-    # 畫框 start
-    # polygon_coords = np.array(counting_regions[0]["polygon"].exterior.coords, dtype=np.int32) #藍色框
-    # cv2.polylines(frame, [polygon_coords], isClosed=True, color=counting_regions[0]['region_color'], thickness=region_thickness)
-    #  畫框 end
-    # frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
-
 cv2.imwrite(output_path, frame)
 cv2.imshow('Image Detection', frame)
 cv2.waitKey(0)
